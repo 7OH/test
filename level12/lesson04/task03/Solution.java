e1926da069f0750947331b8b777fc5099a42b44f
7OH <pavlyukoff@gmail.com>
2015-03-26 16:03:41 +0200

    Lesson 04

---
+++ level12/lesson04/task03/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.test.level12.lesson04.task03;
+
+/* Пять методов print с разными параметрами
+Написать пять методов print с разными параметрами.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public static void print(int toPrint)
+    {
+        System.out.println(toPrint);
+    }
+
+    public static void print(String toPrint)
+    {
+        System.out.println(toPrint);
+    }
+
+    public static void print(char toPrint)
+    {
+        System.out.println(toPrint);
+    }
+
+    public static void print(byte toPrint)
+    {
+        System.out.println(toPrint);
+    }
+
+    public static void print(boolean toPrint)
+    {
+        System.out.println(toPrint);
+    }
+}
